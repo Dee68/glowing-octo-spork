c54e74710ae8695fe81d021b90bdc401a3c2e97a
squarebaba <dimieripou1432@gmail.com>
2021-10-29 19:39:53 +0300

    product views modified

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
+from .import views
 
 app_name='products'
 
 urlpatterns = [
-    path(),
+    path('', views.index, name='products'),
+    path('<int:id>/<str:slug>/',views.product_details, name='product_details'),
 
     ]
--- product/views.py
+++ product/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import *
 # Create your views here.
 
 # all products
 def index(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products':products}
     return render(request, 'product/index.html', context)
+
+def product_details(request, id, slug):
+   
+    pictures = Picture.objects.filter(title__contains='slider')
+    product = get_object_or_404(Product,id=id, slug=slug)
+    pcategories = Category.objects.filter(parent=None)
+    ppictures = Picture.objects.filter(product=product)
+    context = {'product':product,'pcategories':pcategories,
+    'ppictures':ppictures,'pictures':pictures}
+    return render(request, 'products/product_details.html',context)
