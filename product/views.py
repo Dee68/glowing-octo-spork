ff2036248dd12f61daa43da9029498fc09ad15a4
squarebaba <dimieripou1432@gmail.com>
2021-11-04 14:27:13 +0200

    view corrected

--- product/views.py
+++ product/views.py
@@ -82,7 +82,7 @@ def add_to_cart(request):
             
     else:
         # Anonymouse user
-        device = request.COOKIES['device']
+        device = request.COOKIES.get('device')
         customer,created = Customer.objects.get_or_create(device=device)
         # customer.id = request.user.id
         print(customer.id, request.user.id)
