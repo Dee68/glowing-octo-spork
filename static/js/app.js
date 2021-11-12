5a24445975ffe32b6536cd1e1f1aea75d18ed4d9
squarebaba <dimieripou1432@gmail.com>
2021-11-12 21:19:55 +0200

    app.js changed

--- static/js/app.js
+++ static/js/app.js
@@ -8,22 +8,3 @@
 //         localStorage.clear();
 //         localStorage.setItem('stepTime', now);
 //     }
-//
-
-window.onload = function () {
-    //cart box
-    const cartBtn = document.querySelector("#cartBtn");
-
-    //console.log(cartBtn.firstChild);
-    const cartBox = document.querySelector(".cartBox");
-    cartBtn.addEventListener("click", function (e) {
-        cartBox.classList.add('active');
-    });
-
-}
-
-if (typeof (Storage) !== 'undefined') {
-    console.log("local storage is working");
-} else {
-    console.log("local storge not working");
-}
