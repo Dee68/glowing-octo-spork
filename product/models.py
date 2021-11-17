b08e1a77a65eda0017a3f8f6e14183c592c12bc5
squarebaba <degberi001@gmail.com>
2021-11-17 19:33:07 +0200

    in_stock field added to product

--- product/models.py
+++ product/models.py
@@ -59,6 +59,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=12, decimal_places=2)
     amount = models.IntegerField()
     miniamount = models.IntegerField()
+    in_stock = models.IntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
