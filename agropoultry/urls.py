510704f4dfb4cc244da24615237f7f15f6a673ee
squarebaba <degberi001@gmail.com>
2021-12-04 12:57:10 +0200

    password reset completed

--- agropoultry/urls.py
+++ agropoultry/urls.py
@@ -44,10 +44,18 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('social-auth/', include('social_django.urls', namespace="social")),
     # authentication views from django
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('reset_password/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset_password/', 
+    auth_views.PasswordResetView.as_view(template_name='account/password_reset.html'), 
+    name='password_reset'),
+    path('reset_password/done/', 
+    auth_views.PasswordResetDoneView.as_view(template_name='account/password_reset_sent.html'), 
+    name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', 
+    auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset_form.html'), 
+    name='password_reset_confirm'),
+    path('reset_password_complete/', 
+    auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_complete.html'), 
+    name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
