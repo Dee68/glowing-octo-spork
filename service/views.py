15a8f0cc53c1f0b1e5e5cbe7f11ee76e7b88c1b5
squarebaba <degberi001@gmail.com>
2021-12-09 12:32:06 +0200

    services corrected

--- service/views.py
+++ service/views.py
@@ -7,21 +7,21 @@ from .models import Service
 
 def index(request):
     setting = Setting.objects.get(pk=1)
-    userprofile = get_object_or_404(UserProfile, user=request.user)
+    # userprofile = get_object_or_404(UserProfile, user=request.user)
     pictures = Picture.objects.filter(title__contains='slider')
     # pcat = get_object_or_404(Category, slug=category_slug)
     pcategories = Category.objects.filter(parent=None)
     services = Service.objects.all()
     # categories = Category.objects.filter(parent=pcat)
     ctx = {'setting':setting,'pcategories':pcategories,'pictures':pictures,
-    'services':services,'userprofile':userprofile}
+    'services':services}
     return render(request,'services/index.html',ctx)
 
 def service_detail(request, slug):
     setting = Setting.objects.get(pk=1)
-    userprofile = get_object_or_404(UserProfile, user=request.user)
+    # userprofile = get_object_or_404(UserProfile, user=request.user)
     pictures = Picture.objects.filter(title__contains='slider')
     pcategories = Category.objects.filter(parent=None)
     service = get_object_or_404(Service, slug=slug)
     return render(request,'services/service_detail.html',{'service':service,'pcategories':pcategories,
-    'pictures':pictures,'setting':setting,'userprofile':userprofile})
+    'pictures':pictures,'setting':setting})
