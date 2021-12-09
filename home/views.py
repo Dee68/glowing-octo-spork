fa51876fa119efc7fcfdd4da052c56066078ba57
squarebaba <degberi001@gmail.com>
2021-12-09 12:01:55 +0200

    about/contact pages chngd

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from .forms import ContactForm,SubscribersForm
 from . models import ContactMessage, Setting, SubscribedUser
 from product.models import *
+from account.models import UserProfile
 from django_pandas.io import read_frame
 # Create your views here.
 def home(request):
@@ -11,6 +12,7 @@ def home(request):
     products = Product.objects.all()
     pcategories = Category.objects.filter(parent=None)
     setting = Setting.objects.get(pk=1)
+    userprofile = get_object_or_404(UserProfile, user=request.user)
     
     if request.method == "POST":
         form = SubscribersForm(request.POST)
@@ -29,7 +31,7 @@ def home(request):
     else:
         form = SubscribersForm() 
     context = {'form':form,'products':products, 'pictures':pictures, 
-    'pcategories':pcategories,'setting':setting}
+    'pcategories':pcategories,'setting':setting,'userprofile':userprofile}
     return render(request, 'home/index.html', context)
 
 
@@ -38,6 +40,7 @@ def contactUs(request):
     products = Product.objects.all()
     pcategories = Category.objects.filter(parent=None)
     setting = Setting.objects.get(pk=1)
+    userprofile = get_object_or_404(UserProfile, user=request.user)
     if request.method == "POST":# check for button click
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -51,7 +54,8 @@ def contactUs(request):
             messages.success(request,"Message successfully sent, we will get back to you soon.")
             return HttpResponseRedirect('/contact')
     form = ContactForm
-    context = {'form':form,'products':products,'pcategories':pcategories,'setting':setting}
+    context = {'form':form,'products':products,'pcategories':pcategories,
+    'setting':setting,'userprofile':userprofile}
     return render(request, 'home/contact.html', context)
 
 # about us page
@@ -59,5 +63,7 @@ def aboutUs(request):
     products = Product.objects.all()
     pcategories = Category.objects.filter(parent=None)
     setting = Setting.objects.get(pk=1)
-    context = {'products':products,'pcategories':pcategories,'setting':setting}
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    context = {'products':products,'pcategories':pcategories,'setting':setting,
+    'userprofile':userprofile}
     return render(request, 'home/about.html', context)
